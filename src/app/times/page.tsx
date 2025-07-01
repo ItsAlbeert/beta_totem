c211f90192cb78b8daae37780dccf8a1ad8ce009
Alberto González <albertogn1997mp@gmail.com>
2025-07-01 23:45:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/times/page.tsx
+++ src/app/times/page.tsx
@@ -111,7 +111,7 @@ export default function TimesPage() {
         extraGameDetailedStatuses: defaultExtraStatuses,
       });
     }
-  }, [editMode, scoreToEdit, games, form.reset]);
+  }, [editMode, scoreToEdit, games]);
 
 
   const addScoreMutation = useMutation({
